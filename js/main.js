17d5dfff0414dbe937b9c62244379eb00ea40e25
Mauricio Silveira <mauricio.machado.silveira@gmail.com>
2017-12-11 16:07:57 -0500

    Change XHR for fetch

--- js/main.js
+++ js/main.js
@@ -4,21 +4,21 @@
   var cars = document.querySelectorAll('.data-ref');
   const httpRequest = new XMLHttpRequest();
 
-  function processRequest() {
-   let reqIndicator = document.querySelector('.request-state');
-   reqIndicator.textContent = httpRequest.readyState;
-
-   if (httpRequest.readyState === XMLHttpRequest.DONE) {
-     if (httpRequest.status === 200) { // 200 means everything is awesome
-      //  debugger;
-      let data = JSON.parse(httpRequest.responseText);
-      processResult(data);
-     }
-     else {
-       alert('There was a problem with the request.');
-     }
-    }
-  }
+  // function processRequest() {
+  //  let reqIndicator = document.querySelector('.request-state');
+  //  reqIndicator.textContent = httpRequest.readyState;
+  //
+  //  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+  //    if (httpRequest.status === 200) { // 200 means everything is awesome
+  //     //  debugger;
+  //     let data = JSON.parse(httpRequest.responseText);
+  //     processResult(data);
+  //    }
+  //    else {
+  //      alert('There was a problem with the request.');
+  //    }
+  //   }
+  // }
 
   function processResult(data) {
     const {modelName, pricing, modelDetails} = data;
@@ -34,15 +34,26 @@
     document.querySelector(`#${data.model}`).classList.remove('nonActive');
   }
 
-  function getCarData() {
-    if (!httpRequest) {
-      alert('browser fail');
-      return false;
-    }
+  // function getCarData() {
+  //   if (!httpRequest) {
+  //     alert('browser fail');
+  //     return false;
+  //   }
+  //
+  //   httpRequest.onreadystatechange = processRequest;
+  //   httpRequest.open('GET', './includes/functions.php?carModel='+this.id);
+  //   httpRequest.send();
+  // }
 
-    httpRequest.onreadystatechange = processRequest;
-    httpRequest.open('GET', './includes/functions.php?carModel='+this.id);
-    httpRequest.send();
+  function getCarData() {
+    const url = './includes/functions.php?carModel=' + this.id;
+
+    fetch(url) // do an ajax call with fetch
+      .then((resp) => resp.json()) // convert to json
+      .then((data) => { processResult(data);}) // call the process function
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 
   getCarData.call(document.querySelector('.data-ref'));
